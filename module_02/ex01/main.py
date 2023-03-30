d14a1303854b152094212a09a0664cd2620298a7
Yorgo Petsas <yorgopecas@gmail.com>
2023-03-30 16:05:44 +0200

    A program that displays an N amount of provided variables

---
+++ module_02/ex01/main.py
@@ -0,0 +1,48 @@
+def what_are_the_vars(*args, **kwargs):
+    obj = ObjectC()
+
+    for key, value in kwargs.items():
+        setattr(obj, key, value)
+
+    for i, element in enumerate(args):
+        var_name = f"var_{i}"
+        if hasattr(obj, var_name):
+            return None
+        setattr(obj, var_name, element)
+
+    return obj
+
+
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars(None, [])
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi") 
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world") 
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, "Yes", a=10, var_2="world")
+    doom_printer(obj)
