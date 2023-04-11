dad6f3555f634756c8ba7b1fbea6bb08dd853e7a
Yorgo Petsas <yorgopecas@gmail.com>
2023-04-11 17:41:25 +0200

    Final Version

--- module_00/ex05/kata01.py
+++ module_00/ex05/kata01.py
@@ -2,7 +2,6 @@ kata = {
     'Python': 'Guido van Rossum',
     'Ruby': 'Yukihiro Matsumoto',
     'PHP': 'Rasmus Lerdorf',
-    'PHP2': 'ME',
 }
 
 for key, value in kata.items():
--- module_00/ex05/kata04.py
+++ module_00/ex05/kata04.py
@@ -1,18 +1,22 @@
 from decimal import Decimal
 
-kata = (11, 4, 132.42222, 10000, 12345.67)
+kata = (0, 4, 132.42222, 10000, 12345.67)
 
-if kata[0] < 10:
-    print(f'module_0{kata[0]}', end=', ')
-else:
-    print(f'module_{kata[0]}', end=', ')
-if kata[1] < 10:
-    print(f'ex_0{kata[1]}', end=' : ')
-else:
-    print(f'ex_{kata[1]}', end=' : ')
-print(round(kata[2], 2), end='')
+def yz_print(kata):
+    if kata[0] < 10:
+        print(f'module_0{kata[0]}', end=', ')
+    else:
+        print(f'module_{kata[0]}', end=', ')
+    if kata[1] < 10:
+        print(f'ex_0{kata[1]}', end=' : ')
+    else:
+        print(f'ex_{kata[1]}', end=' : ')
+    print(round(kata[2], 2), end='')
 
-for i in range(2):
-    data = kata[3 + i]
-    data = Decimal(data)
-    print(", %.2E" % data, end='')
+    for i in range(2):
+        data = kata[3 + i]
+        data = Decimal(data)
+        print(", %.2E" % data, end='')
+    print()
+
+yz_print(kata)
