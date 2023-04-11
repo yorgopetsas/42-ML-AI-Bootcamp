ab80ec82097cb373ad5400cbd4045cd2d552370b
Yorgo Petsas <yorgopecas@gmail.com>
2023-04-11 17:03:49 +0200

    Improved Error Handling

--- module_00/ex02/whois.py
+++ module_00/ex02/whois.py
@@ -1,14 +1,28 @@
 import sys
 
-x = sys.argv[1]
 
-if (x.isdigit() is False):
-    print("\nPlease provide a whole number. ", end='')
-    print("No decimal separators, float nor strings allowed)\n")
-    exit()
-if int(x) == 0:
-    print("\nThis is 0")
-elif (int(x) % 2 == 0):
-    print("\nThe number is Even\n")
+def whois(x):
+
+    if (x.isdigit() is False):
+        print("\nPlease provide a whole number. ", end='')
+        print("No decimal separators, float nor strings allowed)\n")
+        exit()
+    if int(x) == 0:
+        print(f"\nThe number {x} is Even.")
+    elif (int(x) % 2 == 0):
+        print(f"\nThe number {x} is Even.")
+    else:
+        print(f"\nThe number {x} is Odd.")
+
+if len(sys.argv) < 2:
+    x = input("\nPlease provide a number: ")
+    print(x)
+    whois(x)
+
+elif len(sys.argv) > 2:
+    x = input("\nPlease provide only one number: ")
+    whois(x)
+
 else:
-    print("\nThe number is Odd\n")
+    x = sys.argv[1]
+    whois(sys.argv[1])
