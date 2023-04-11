07f8d216bfc1f0d94c6e628f279cf1567830ddb6
Yorgo Petsas <yorgopecas@gmail.com>
2023-04-11 17:31:52 +0200

    Improved error handling

--- module_00/ex04/operations.py
+++ module_00/ex04/operations.py
@@ -1,9 +1,9 @@
 import sys
 
 if (len(sys.argv) != 3):
-    print("Error: Please provide exactly 2 numbers. No other input accepted!")
+    print("\nError: Please provide exactly 2 numbers. No other input accepted!\n\nUsage: python operations.py <number1> <number2>\nExample:\npython operations.py 10 3\n")
 
-if (sys.argv[1].isdigit() and sys.argv[2].isdigit()) and (len(sys.argv) == 3) or (sys.argv[1][0] == "-" or sys.argv[2][0] == "-" and sys.argv[1][1::].isdigit() and sys.argv[2][1::].isdigit()):
+elif (sys.argv[1].isdigit() and sys.argv[2].isdigit()) and (len(sys.argv) == 3) or (sys.argv[1][0] == "-" or sys.argv[2][0] == "-" and sys.argv[1][1::].isdigit() and sys.argv[2][1::].isdigit()):
     a = int(sys.argv[1])
     b = int(sys.argv[2])
 
@@ -14,5 +14,3 @@ if (sys.argv[1].isdigit() and sys.argv[2].isdigit()) and (len(sys.argv) == 3) or
     else:
         print(f'Quotient is {a / b}')
         print(f'Remainder is {a % b}')
-else:
-    print("Error: Please provide exactly two numbers/arguments. No other input accepted!")
