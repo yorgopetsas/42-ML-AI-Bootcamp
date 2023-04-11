96694a7049af53841877f5d46be4cea17670d36d
Yorgo Petsas <yorgopecas@gmail.com>
2023-04-11 22:42:28 +0200

    Stable Version 1. Menu, Recipe Creation, Recipe Listing, Cookbook Listing & Recipe Deletion working

--- module_00/ex06/recipe.py
+++ module_00/ex06/recipe.py
@@ -2,7 +2,23 @@ import os
 
 os.system('clear')
 
-cookbook = [{'name' : 'bocadillo','ingredients': ['jamón', 'pan', 'queso', 'tomate'],'meal': "almuerzo",'prep_time': 10}, {'name' : 'tarta', 'ingredients': ['harina', 'azúcar', 'huevos'],'meal': "postre",'prep_time': 60}, {'name' : 'salad','ingredients': ['aguacate', 'rúcula', 'tomates', 'espinacas'],'meal': "almuerzo",'prep_time': 15}]
+cookbook = [
+        {
+            'name' : 'bocadillo',
+            'ingredients': ['jamón', 'pan', 'queso', 'tomate'],
+            'meal': "almuerzo",'prep_time': 10
+        }, 
+        {
+            'name' : 'tarta',
+            'ingredients': ['harina', 'azúcar', 'huevos'],
+            'meal': "postre",'prep_time': 60
+        }, 
+        {
+            'name' : 'salad',
+            'ingredients': ['aguacate', 'rúcula', 'tomates', 'espinacas'],
+            'meal': "almuerzo",'prep_time': 15
+        }
+    ]
 
 
 def addRecipe():
