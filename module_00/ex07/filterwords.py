3d05551443ce9414043573520a39efe2cfded565
Yorgo Petsas <yorgopecas@gmail.com>
2023-04-11 23:27:50 +0200

    Final Version. Errors related to list creation and last letter fixed

--- module_00/ex07/filterwords.py
+++ module_00/ex07/filterwords.py
@@ -4,26 +4,28 @@ import string
 
 if len(sys.argv) == 3:
     text = sys.argv[1]
+    text_len = len(text)
     try:
         limit = int(sys.argv[2])
     except Exception:
         print("Second Argument not an Integer")
+    words = []
     word = ''
-    for i in text:
-        if i in string.punctuation:
+    for i, l in enumerate(text):
+        if l in string.punctuation:
             continue
-        elif i is ' ':
+        elif l == ' ':
             if len(word) > limit:
-                print(word)
+                words.append(word)
                 word = ''
                 continue
             else:
                 word = ''
                 continue
-        word = word + i
-        # print(word)
-    if len(word) > limit:
-        print(word)
+        word = word + l
+        if i == text_len - 1 and len(word) > limit:
+            words.append(word)
+    print(words)
 
 else:
     print('AssertionError: wrong number of arguments')
