5fe85e9ae57a86103cef4c7de9e1683e922e8887
Yorgo Petsas <yorgopecas@gmail.com>
2023-04-18 23:00:17 +0200

    ex03 up

---
+++ module_02/ex03/csvreader.py
@@ -0,0 +1,50 @@
+import csv
+
+class CsvReader:
+    def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+        self.filename = filename
+        self.sep = sep
+        self.header_state = header
+        self.skip_top = skip_top
+        self.skip_bottom = skip_bottom
+        self.file_obj = None
+        self.fulldata = []
+        self.header = []
+
+
+    def __enter__(self):
+        self.file_obj = open(self.filename, mode="r", encoding="utf-8")
+
+        for i, line in enumerate(self.file_obj):
+            self.fulldata.append(list(map(str.strip, line.split(self.sep))))
+
+        if all(len(elem) == len(self.fulldata[0]) for elem in self.fulldata):
+            return self
+        else:
+            return None
+
+
+    def __exit__(self, type, value, traceback):
+        print('Exit')
+        self.file_obj.close()
+    
+
+    def getdata(self):
+        start = self.skip_top
+        end = len(self.fulldata) - self.skip_bottom
+        if self.header_state == True:
+            self.header = self.fulldata[0]
+        if self.header_state == True:
+            tmp = self.fulldata[ start + 1 : end ]
+            tmp.insert(0, self.header)
+            self.fulldata = tmp
+            return self.fulldata
+        else:
+            return self.fulldata[ start : end ]
+
+
+    def getheader(self):
+        if self.header_state == True:
+            return(self.fulldata[0])
+        else:
+            return None
