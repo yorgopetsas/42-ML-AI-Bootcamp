1a4c784812998073a5437af547a3fc11091dd37e
Yorgo Zisis petsas <yzisis-p@car2s5.42barcelona.com>
2023-05-23 23:56:46 +0200

    ex04 Started

--- module_03/ex04/Kmeans.py
+++ module_03/ex04/Kmeans.py
@@ -1,3 +1,60 @@
+import numpy as np
+
+import csv
+
+class CsvReader:
+    def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+        self.filename = filename
+        self.sep = sep
+        self.header_state = header
+        self.skip_top = skip_top
+        self.skip_bottom = skip_bottom
+        self.file_obj = None
+        self.fulldata = []
+        self.header = []
+
+
+    def __enter__(self):
+        self.file_obj = open(self.filename, mode="r", encoding="utf-8")
+
+        for i, line in enumerate(self.file_obj):
+            self.fulldata.append(list(map(str.strip, line.split(self.sep))))
+
+        if all(len(elem) == len(self.fulldata[0]) for elem in self.fulldata):
+            return self
+        else:
+            return None
+
+
+    def __exit__(self, type, value, traceback):
+        # print('Exit')
+        self.file_obj.close()
+    
+
+    def getdata(self):
+        start = self.skip_top
+        end = len(self.fulldata) - self.skip_bottom
+        if self.header_state == True:
+            self.header = self.fulldata[0]
+        if self.header_state == True:
+            tmp = self.fulldata[ start + 1 : end ]
+            tmp.insert(0, self.header)
+            self.fulldata = tmp
+            return self.fulldata
+        else:
+            return self.fulldata[ start : end ]
+
+
+    def getheader(self):
+        if self.header_state == True:
+            return(self.fulldata[0])
+        else:
+            return None
+
+
+
+
+
 
 
 class KmeansClustering:
@@ -24,6 +81,12 @@ class KmeansClustering:
 		This function should not raise any Exception.
 		"""
 
+		print('ohhh yeah')
+
+
+def get_dist(self, entries, point_1, point_2):
+		pass
+
 
 	def predict(self, X):
 		"""
@@ -38,3 +101,22 @@ class KmeansClustering:
 		-------
 		This function should not raise any Exception.
 		"""
+
+
+if __name__ == "__main__":
+
+
+	k = KmeansClustering()
+
+
+	filepath='solar_system_census.csv' 
+	ncentroid=4
+	max_iter=30
+
+
+	with CsvReader("solar_system_census.csv", header=True) as file:
+		data = np.array(file.getdata())
+            
+                
+	# k.get_dist(self, entries, point_1, point_2):
+	# k.fit(data.astype(np.float))
