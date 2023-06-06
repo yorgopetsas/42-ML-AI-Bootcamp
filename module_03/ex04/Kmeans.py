747aa5398c31f2d516c58f0afd304780efc39f62
Yorgo Petsas <yorgopecas@gmail.com>
2023-06-06 15:48:17 +0200

    Method for mean value finished

--- module_03/ex04/Kmeans.py
+++ module_03/ex04/Kmeans.py
@@ -10,6 +10,14 @@ import csv
 ########################################################################
 
 
+# class Points:
+# 	def __init__(self, id, x, y, z):
+# 		self.id = id
+# 		self.x = x
+# 		self.y = y
+# 		self.z = z
+
+
 class CsvReader:
     def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
         self.filename = filename
@@ -25,8 +33,10 @@ class CsvReader:
     def __enter__(self):
         self.file_obj = open(self.filename, mode="r", encoding="utf-8")
 
+
         for i, line in enumerate(self.file_obj):
             self.fulldata.append(list(map(str.strip, line.split(self.sep))))
+
 
         if all(len(elem) == len(self.fulldata[0]) for elem in self.fulldata):
             return self
@@ -69,25 +79,24 @@ class KmeansClustering:
 		self.centroids = [] # values of the centroids
 
 
-	def get_mean(self, source, data):
-
+	def get_mean(self, values, data):
 
 		x=y=z=0
 		cnt = 0
-		for d in data:
-			if d[0] in source:
-				print(d)
-				# print(d[1],d[2],d[3])
-				# x+=d[1]
-				# y+=d[2]
-				# z+=d[3]
-				# cnt+=1
+		md = 0
+		
+		for l in data:
+			if cnt > 0:
+				for v in values:
+					if int(l[0]) == v:
+						x+=float(l[1])
+						y+=float(l[2])
+						z+=float(l[3])
+						md +=1
+			cnt+=1
 
-
-		# res = [x/cnt, y/cnt, z/cnt]
-		res = "blah"
-
-
+		id = 1000 + md
+		res = [f'{id}', f'{x/md}', f'{y/md}', f'{z/md}']
 		return(res)
 
 
@@ -108,7 +117,7 @@ class KmeansClustering:
 		return(dis)
 
 
-	def fit(self, X):
+	def fit(self, X, centro=None):
 		"""
 		Run the K-means clustering algorithm.
 		For the location of the initial centroids, random pick ncentroids from the dataset.
@@ -124,22 +133,36 @@ class KmeansClustering:
 		"""
 
 
-		starting_centroids = random.sample(range(0, (len(X)+1) + 1), 4)
+		# Temp fixed centroides
+		starting_centroids = [32, 12, 36, 58]
+		
+		# Randonmy chose a number in the range of the available IDs
+		# starting_centroids = random.sample(range(0, (len(X)+1) + 1), 4)
+
+
 		print(starting_centroids)
 
-
-		# Have to Normalize data:
-		values = data[1:, 1:].astype(float)
+		
+		# !!! 
+		# Here I have to normalize the data:
+		values = X[1:, 1:].astype(float)
+		
+		# print(values)
 		data_2 = []
 
 
-		# Create data frame with the centroids
-		for zz in data:
-			try:
-				if int(zz[0]) in starting_centroids:
-					data_2.append(zz.tolist())
-			except:
-				pass
+		# # Create data frame with the centroids
+		if centro == None:
+			for zz in X:
+				try:
+					if int(zz[0]) in starting_centroids:
+						data_2.append(zz.tolist())
+				except:
+					pass
+		else:
+			data_2 = centro
+		# print(data_2)
+		# print()
 
 
 		dicts = {}
@@ -150,8 +173,8 @@ class KmeansClustering:
 			dicts[elem] = []
 
 
-		# Calculate shortest distance
-		for i,zz in enumerate(data):
+		# # Calculate shortest distance
+		for i,zz in enumerate(X):
 			if i == 0:
 				continue
 			res = 10000000000000000000000000000
@@ -161,22 +184,21 @@ class KmeansClustering:
 				if distance < res:
 					res = distance
 					tmp_id = sp[0]
-			# print(f"Smallest Distance {zz[0]} is {res} and is for {tmp_id}")
 			dicts[int(tmp_id)].append(int(zz[0]))
 
 
+		meanz = []
+
+
+		print(dicts)
+
+
 		for k,v in dicts.items():
-			print(k,v)
-			meanz = self.get_mean(v, data)
-			print(meanz)
+			meanz.append(self.get_mean(v, X))
+		print(meanz)
 
 
-
-		# for every point in the array I have to calculate the difference to each centroid and return the smallest
-		#
-		# scaler = MinMaxScaler()
-		# normalized_values = scaler.fit_transform(values)
-		# data[1:, 1:] = normalized_values
+		# self.fit(data, meanz)
 
 
 	def predict(self, X):
@@ -207,23 +229,19 @@ if __name__ == "__main__":
 
 	with CsvReader("code.csv", header=True) as file:
 		data = np.array(file.getdata())
-            
-                
+
+
 	# k.get_dist(self, entries, point_1, point_2):
 	k.fit(data)
-	# k.get_dist(a,b)
+
 
 
 #########
 # END
-########
-	# Create dictionary for each centroid
-	# for it in data_2:
-	# 	dict_name = "di_" + str(it[0])
-	# 	# Create dictionary with key as item value
-	# 	globals()[dict_name] = {it[0]: None}
-	# 	print(dict_name)
+#########
 
-	# for it in data_2:
-	# 	print(type('di_' + str(it[0])))
-	# print(dicts)
+		# for every point in the array I have to calculate the difference to each centroid and return the smallest
+		#
+		# scaler = MinMaxScaler()
+		# normalized_values = scaler.fit_transform(values)
+		# data[1:, 1:] = normalized_values
